a905b67ed45af28f844da0a0e64622d74f89016d
Edwin <edwinpoueriet.dev@gmail.com>
2022-11-01 12:20:55 -0400

    main layout without much detail

--- my-solution/src/App.tsx
+++ my-solution/src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import './App.css'
 function App() {
-  const [markdown, setMarkdown]= useState('')
+  const [markdown, setMarkdown]= useState<string>('');
   return (
-    <div>
-      <textarea value={markdown} onChange={(e) => setMarkdown(e.target.value)} rows={5} />
-      <ReactMarkdown>{markdown}</ReactMarkdown>
+    <>
+    <div className="container">
+      <div className="header">title</div>
+      <div className="content">
+        <textarea className="mainInput" value={markdown} onChange={(e) => setMarkdown(e.target.value)} rows={5} />
+        <ReactMarkdown className="markdownResult">{markdown}</ReactMarkdown>
+      </div>
     </div>
+    </>
   );
 }
 
