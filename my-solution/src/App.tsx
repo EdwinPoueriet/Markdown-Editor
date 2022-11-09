5e70aeeaf426811554384ee30491d78389075fbc
Edwin <edwinpoueriet.dev@gmail.com>
2022-11-09 00:17:13 -0400

    added new dependencies and changing css pattern

--- my-solution/src/App.tsx
+++ my-solution/src/App.tsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
-import ReactMarkdown from "react-markdown";
-import './App.css'
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './App.css';
 function App() {
-  const [markdown, setMarkdown]= useState<string>('');
+  const [markdown, setMarkdown] = useState<string>('');
   return (
     <>
-    <div className="container">
-      <div className="header">Edwin's Markdown</div>
-      <div className="content">
-        <textarea autoFocus className="mainInput" value={markdown} onChange={(e) => setMarkdown(e.target.value)} rows={5} />
-        <ReactMarkdown className="markdownResult">{markdown}</ReactMarkdown>
+      <div className="container">
+        <div className="header">Edwin's Markdown</div>
+        <div className="content">
+          <textarea
+            autoFocus
+            className="markdown markdown-input"
+            value={markdown}
+            onChange={(e) => setMarkdown(e.target.value)}
+            rows={5}
+          />
+          <ReactMarkdown  className="markdown">{markdown}</ReactMarkdown>
+        </div>
       </div>
-    </div>
     </>
   );
 }
